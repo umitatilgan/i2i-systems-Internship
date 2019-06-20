cd2aa0c7cc5b26a6ebc12f38af7b152b0e9ffd7f
Ümit <41282469+umitatilgan@users.noreply.github.com>
2019-06-20 16:27:56 +0300

    Create table.sql

---
+++ table.sql
@@ -0,0 +1,28 @@
+create table intern
+(
+Name varchar(15),
+Surname varchar(15),
+unýversty_name varchar(50),
+department varchar(30)
+
+)
+
+
+Insert Into intern (Name,Surname,unýversty_name,department) values('Ümit','Atýlgan','Yýldýz Technical University','Mathematical Engineering');
+Insert Into intern (Name,Surname,unýversty_name,department) values('Samet','Atýlgan','YMarmara University','Computer Engineering');
+Insert Into intern (Name,Surname,unýversty_name,department) values('Ali','Veli','Yýldýz Technical University','Civil Engineering');
+Insert Into intern (Name,Surname,unýversty_name,department) values('Ahmet','Arslan','Ýstanbul University','Chemistry');
+Insert Into intern (Name,Surname,unýversty_name,department) values('Selim','Sarý','YKocaeli University','Computer');
+Insert Into intern (Name,Surname,unýversty_name,department) values('Esma','Yýlmaz','sakarya University','Software Engineering');
+Insert Into intern (Name,Surname,unýversty_name,department) values('Hacý','Kaya','Ýstanbul University','Teacher');
+Insert Into intern (Name,Surname,unýversty_name,department) values('Leyla','Bayram','Yýldýz Technical University','Teacher');
+Insert Into intern (Name,Surname,unýversty_name,department) values('Mehmet','Varol','Ýstabbul Technical University','Environmental Engineering');
+Insert Into intern (Name,Surname,unýversty_name,department) values('Ýsmail','Demir','Balýkesir University','Mathematical');
+Insert Into intern (Name,Surname,unýversty_name,department) values('Mehmet','Varol','Ýstabbul Technical University','Environmental Engineering');
+Insert Into intern (Name,Surname,unýversty_name,department) values('Orhan','Beylen','Balýkesir University','Teacher');
+
+SELECT * FROM intern WHERE department= 'Mathematical Engineering';
+
+SELECT * From intern 
+
+SELECT Name,Surname FROM intern 
