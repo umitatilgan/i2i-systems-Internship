aa3b073fbcb1e99932888ba3aeb47c38665df4ae
Ümit <41282469+umitatilgan@users.noreply.github.com>
2019-06-22 13:33:10 +0300

    Update commission.sql

--- commission.sql
+++ commission.sql
@@ -1,3 +1,5 @@
+--A report that displays all employees, and indicate with the words Yes or No whether they receive a commission--
+
 SELECT LAST_NAME,SALARY,
 DECODE(COMMISSION_PCT, NULL, 'NO','YES')
 COMMISSION
