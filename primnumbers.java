9515a64f23fb89230ae2bface51fd6387a8a74af
Ümit <41282469+umitatilgan@users.noreply.github.com>
2019-06-22 19:19:21 +0300

    Create primnumbers.java

---
+++ primnumbers.java
@@ -0,0 +1,27 @@
+//all prime numbers between 1 and 20
+package primesnumber;
+import java.util.Scanner;
+
+public class PrimeNumber {
+	public static void main(String[] args) {        
+
+        
+        int control = 0;
+        
+        for(int i=2; i<=20;i++){
+            
+            for(int j=2;j<i;j++){                
+
+                if(i%j == 0){
+                    control = 1;
+                }
+            }
+
+            if(control == 0){
+               System.out.println(i);
+            }else{
+               control = 0; 
+            }     
+        }
+    }	
+}
