8cdbfb5de3e4c4b06b72232899bedb9e3f95496d
Ümit <41282469+umitatilgan@users.noreply.github.com>
2019-07-02 18:29:32 +0300

    Add files via upload

---
+++ Weather/WebApplication3/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        public ActionResult HavaDurumu()
+        {
+            string api = "&APPID=6bdac780aa3d97cb92684f2131b53697";
+            string baglanti = "https://api.openweathermap.org/data/2.5/weather?q=Istanbul&mode=xml" + api;
+            XDocument Hava = XDocument.Load(baglanti);
+            var sicaklik = Hava.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            var icon = Hava.Descendants("weather").ElementAt(0).Attribute("icon").Value;
+            var durum = Hava.Descendants("weather").ElementAt(0).Attribute("value").Value;
+            ViewBag.icon = "http://openweathermap.org/img/w/" + icon + ".png";
+            ViewBag.sicaklik = sicaklik + " ºC";
+            ViewBag.durum = durum;
+
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
